2c8facd5fa75a20829a25bbe057e25ef628f5f54
artful01 <rais.ilias@ensam-casa.ma>
2023-03-16 06:38:31 -0700

    s

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Prints a line of code
+*Return: always 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char: %d",sizeof(char));
+printf("Size of a int: %d",sizeof(int));
+printf("Size of a long int: %d",sizeof(long int));
+printf("Size of a long long int: %d",sizeof(long long int));
+printf("Size of a float: %d",sizeof(float));
+ return (0);
+}
