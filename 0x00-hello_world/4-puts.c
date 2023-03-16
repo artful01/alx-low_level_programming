6c803fd0bcb9a056fc4a178852fd3b82c8979a59
artful01 <rais.ilias@ensam-casa.ma>
2023-03-16 06:03:42 -0700

    s

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Prints a line of code
+*return always 0
+*/
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+    }
+
