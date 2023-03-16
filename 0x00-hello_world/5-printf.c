fe2288415780b826e409fae0052297b3b1133126
artful01 <rais.ilias@ensam-casa.ma>
2023-03-16 06:15:02 -0700

    s

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Prints a line of code
+*Return: always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
