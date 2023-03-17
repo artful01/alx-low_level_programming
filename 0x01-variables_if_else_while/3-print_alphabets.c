4377b19cd212d6078aad8246e34091010f73b75f
artful01 <rais.ilias@ensam-casa.ma>
2023-03-17 04:55:38 -0700

    s

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,12 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	char maj[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
 	  putchar(alp[i]);
-	  putchar(maj[26]);
 	}
 	putchar('\n');
 	return (0);
