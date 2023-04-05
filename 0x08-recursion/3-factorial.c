c742ffb40452d29fa9b3fbd5369e73d11e3ef84c
artful01 <rais.ilias@ensam-casa.ma>
2023-04-05 14:59:46 -0700

    s

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,13 +2,13 @@
 /**
  *factorial - Returns the factorial of a number
  *@n: number to return
- *Return : return factorial of n
+ *Return: factorial of n
  */
 int factorial(int n)
 {
-  if (n < 0)
-    return (-1);
-  if (n == 0)
-    return (1);
-  return (n * factorial(n - 1));
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
 }
