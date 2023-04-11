5ec6c9fb760285f0b5bb5b712dc892057dce4899
artful01 <rais.ilias@ensam-casa.ma>
2023-04-11 07:03:28 -0700

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat -  returns a pointer to a 2 dimensional array of integers
+ * @s1: width of the grid
+ * @s2: height of the grid
+ * Return: Nothing
+ */
+char *str_concat(char *s1, char *s2)
+{
+unsigned int size1 = 0, size2 = 0;
+char *ptr, *ret;
+ptr = s1;
+if (s1)
+{
+while (*ptr++)
+size1++;
+}
+else
+s1 = "";
+ptr = s2;
+if (s2)
+{
+while (*ptr++)
+size2++;
+}
+else
+s2 = "";
+ret = malloc(size1 + size2 + 1);
+if (!ret)
+return (NULL);
+ptr = ret;
+while (*s1)
+*ptr++ = *s1++;
+while (*s2)
+*ptr++ = *s2++;
+*ptr = 0;
+return (ret);
+}
